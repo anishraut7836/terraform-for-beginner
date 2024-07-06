404a6595c031be39eb7bbe4af5001e6ab7214dc1
Anish Kumar <anishraut7836@gmail.com>
2024-07-06 10:42:42 +0530

    dynamic block

--- terraform-associate-course/lab-18-dynamic-block/dynamic-locals/main.tf
+++ terraform-associate-course/lab-18-dynamic-block/dynamic-locals/main.tf
@@ -1,17 +1,30 @@
+locals {
+  ingress_rules = [{
+    port = 443
+    description = "Port 443"
+  },
+  {
+    port = 80
+    description = "Port 80"
+  }
+  
+  ]
+}
+
 
 resource "aws_security_group" "main" {
-  name   = "core-sg-variable"
+  name   = "core-sg-dynamic"
   #vpc_id = aws_vpc.vpc.id
    vpc_id = "vpc-0c769806a661044e5"
 
   dynamic "ingress" {
-    for_each = var.web_ingress
+    for_each = local.ingress_rules
     content {
       description = ingress.value.description
       from_port   = ingress.value.port
       to_port     = ingress.value.port
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
 
   }
